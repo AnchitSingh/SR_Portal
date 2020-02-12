ebb3a03117595e8f0affe621ebd9ff3312067e1f
Anchit Singh <58829755+AnchitSingh@users.noreply.github.com>
2020-02-12 23:14:16 +0530

    Add files via upload

--- flaskblog/static/assets/js/demo.js
+++ flaskblog/static/assets/js/demo.js
@@ -1099,68 +1099,7 @@ demo = {
 			eventLimit: true, // allow "more" link when too many events
 
 
-            // color classes: [ event-blue | event-azure | event-green | event-orange | event-red ]
-            events: [
-				{
-					title: 'All Day Event',
-					start: new Date(y, m, 1),
-                    className: 'event-default'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d-4, 6, 0),
-					allDay: false,
-					className: 'event-rose'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: new Date(y, m, d+3, 6, 0),
-					allDay: false,
-					className: 'event-rose'
-				},
-				{
-					title: 'Meeting',
-					start: new Date(y, m, d-1, 10, 30),
-					allDay: false,
-					className: 'event-green'
-				},
-				{
-					title: 'Lunch',
-					start: new Date(y, m, d+7, 12, 0),
-					end: new Date(y, m, d+7, 14, 0),
-					allDay: false,
-					className: 'event-red'
-				},
-				{
-					title: 'Md-pro Launch',
-					start: new Date(y, m, d-2, 12, 0),
-					allDay: true,
-					className: 'event-azure'
-				},
-				{
-					title: 'Birthday Party',
-					start: new Date(y, m, d+1, 19, 0),
-					end: new Date(y, m, d+1, 22, 30),
-					allDay: false,
-                    className: 'event-azure'
-				},
-				{
-					title: 'Click for Creative Tim',
-					start: new Date(y, m, 21),
-					end: new Date(y, m, 22),
-					url: 'http://www.creative-tim.com/',
-					className: 'event-orange'
-				},
-				{
-					title: 'Click for Google',
-					start: new Date(y, m, 21),
-					end: new Date(y, m, 22),
-					url: 'http://www.creative-tim.com/',
-					className: 'event-orange'
-				}
-			]
+            
 		});
     },
 
