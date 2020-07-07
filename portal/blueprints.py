a64441e0d69422b4671b925695b5b354d5d6e616
Anchit Singh <58829755+AnchitSingh@users.noreply.github.com>
2020-07-07 16:29:45 +0530

    Updates

--- portal/blueprints.py
+++ portal/blueprints.py
@@ -1,5 +1,1 @@
 from portal import app
-from portal.extra_courses.BTECH.routes import BTECHs
-app.register_blueprint(BTECHs)
-from portal.extra_courses.gtech.routes import gtechs
-app.register_blueprint(gtechs)
