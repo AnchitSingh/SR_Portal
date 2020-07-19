547d0059df3009ca8df9f9d1f07e54f95702cd82
Anchit Singh <58829755+AnchitSingh@users.noreply.github.com>
2020-07-20 05:09:22 +0530

    last update

--- flask_app.py
+++
@@ -1,4 +0,0 @@
-from portal import app,socketio
-
-if __name__ == '__main__':
-    socketio.run(app)
--- portal/static/assets/js/annoucements.js
+++
@@ -1,9 +0,0 @@
-var socket = io();
-socket.on('connect',()=>{
-    document.querySelector('#annoucementForm').addEventListener('submit',(e)=>{
-        socket.emit('annoucement','An annoucement has been made')
-    })
-    socket.on('message',(msg)=>{
-        alert(msg)
-    })
-})
